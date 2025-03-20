40d8f48883cfec75690ea94f4b896f986f1aafdd
Oblepixa123 <belaevao910@gmail.com>
2025-03-20 17:18:17 +0300

    adding project file

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import holidays
-print("Данный проект создан для определения празника в России по введеной в него дате")
+
+
+print("Данный проект создан для определения празника в России по введеной дате в консоль.")
 ru_holidays = holidays.RU()
 date_to_check = input("введите дату празника в формате гггг-мм-дд :  ")
 holiday_name = ru_holidays.get(date_to_check)
 if holiday_name:
     print(holiday_name)
 else:
-    print("на указаной вами дате празников нету")
+    print("На указаной вами дате празников нету.")
